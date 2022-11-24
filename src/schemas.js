794a711e96214e2d31e6ead64ff3b05e4ae6c4d0
lcfernandez <lcfernandez@cos.ufrj.br>
2022-11-24 10:39:05 -0300

    feat: create schema user

--- src/schemas.js
+++ src/schemas.js
@@ -10,6 +10,11 @@ export const newUser = joi.object({
     repeat_password: joi.ref("password")
 });
 
+export const user = joi.object({
+    email: joi.string().email().required(),
+    password: joi.string().min(4).required()
+});
+
 export const product = joi.object({
     name: joi.string().min(1).required(),
     price: joi.string().min(2).required(),
